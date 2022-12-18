15cd56d02e67cdb5f5c19c8d4d29e092c689c9ce
andreinegrea14 <andrei.negrea14@yahoo.com>
2022-12-19 01:03:47 +0200

    added backend

---
+++ backend/controllers/register.js
@@ -0,0 +1,70 @@
+const bcrypt = require("bcrypt");
+
+const client = require("../configs/database");
+
+const jwt = require("jsonwebtoken");
+
+//Registration Function
+
+exports.register = async (req, res) => {
+    const { firstname, lastname, email, phonenumber, password, birthdate, bloodtype } = req.body;
+    try {
+        const data = await client.query(`SELECT * FROM users WHERE email= $1;`, [email]); //Checking if user already exists
+        const arr = data.rows;
+        if (arr.length != 0) {
+            return res.status(400).json({
+                error: "Email already there, No need to register again.",
+            });
+        }
+        else {
+            bcrypt.hash(password, 10, (err, hash) => {
+                if (err)
+                    res.status(err).json({
+                        error: "Server error",
+                    });
+                const user = {
+                    firstname,
+                    lastname,
+                    email,
+                    phonenumber,
+                    password: hash,
+                    birthdate,
+                    bloodtype
+                };
+                var flag = 1; //Declaring a flag
+
+                //Inserting data into the database
+
+                client
+                    .query(`INSERT INTO users (firstname, lastname, email, phonenumber, password, birthdate, bloodtype) VALUES ($1,$2,$3,$4,$5,$6,$7);`, [user.firstname, user.lastname, user.email, user.phonenumber, user.password, user.birthdate, user.bloodtype], (err) => {
+
+                        if (err) {
+                            flag = 0; //If user is not inserted is not inserted to database assigning flag as 0/false.
+                            console.error(err);
+                            return res.status(500).json({
+                                error: "Database error"
+                            })
+                        }
+                        else {
+                            flag = 1;
+                            res.status(200).send({ message: 'User added to database, not verified' });
+                        }
+                    })
+                if (flag) {
+                    const token = jwt.sign( //Signing a jwt token
+                        {
+                            email: user.email
+                        },
+                        process.env.SECRET_KEY
+                    );
+                };
+            });
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: "Database error while registring user!", //Database connection error
+        });
+    };
+}
---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express(); //Initialized express
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+
+    res.status(200).send("Engine Started, Ready to take off!");
+
+})
+
+app.listen(port, () => {
+
+    console.log(`Here we go, Engines started at ${port}.`);
+
+})
+
+require("./configs/dotenv");
+const client = require("./configs/database");
+
+client.connect((err) => { //Connected Database
+
+    if (err) {
+
+        console.log(err);
+
+    }
+
+    else {
+
+        console.log("Data logging initiated!");
+    }
+
+});
+
+const  user  =  require("./routes/users");
+
+app.use("/user",  user);  //Route for /user endpoint of API
