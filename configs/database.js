3ef1f80cc21c1d5bfa0f5b64f04a468cd3bd46c8
andreinegrea14 <andrei.negrea14@yahoo.com>
2022-12-19 02:23:39 +0200

    added ssl reject unauthorized flag to db config

--- configs/database.js
+++ configs/database.js
@@ -1,12 +1,9 @@
 const { Client } = require("pg");
 
 const client = new Client({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.DB_URL,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
-
-}); //Configuring PostgresSQL Database
-
+    connectionString: process.env.DB_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
 module.exports = client;
