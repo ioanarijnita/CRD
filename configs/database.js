6599273f963626318751e7836407281370c7d5ab
andreinegrea14 <andrei.negrea14@yahoo.com>
2022-12-19 02:05:31 +0200

    added client config for db with multiple info

--- configs/database.js
+++ configs/database.js
@@ -1,6 +1,12 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-});
-module.exports = pool;
+const { Client } = require("pg");
+
+const client = new Client({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.DB_URL,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
+
+}); //Configuring PostgresSQL Database
+
+module.exports = client;
