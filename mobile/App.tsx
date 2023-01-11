686407a118b63477754d2c165ef16054f59167b3
ioanarijnita <ioanarijnita@gmail.com>
2023-01-11 13:11:47 +0200

    ejected project to expo bare workflow

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
+import MapboxGL from '@rnmapbox/maps';
 import { NativeBaseProvider } from 'native-base';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { SignUpNavigator } from './src/navigators/signup-navigator';
 import { Concept, LoginContext } from './src/screens/common/login';
@@ -18,6 +19,12 @@ export default function App() {
     birthDate: "",
     bloodType: ""
   })
+
+  useEffect(() => {
+    MapboxGL.setWellKnownTileServer('mapbox');
+    MapboxGL.setAccessToken("sk.eyJ1IjoiaW9hbmFyaWpuaXRhIiwiYSI6ImNsY3F5ZXNyazA1Z2Qzb3FpM3RjdGs0aW4ifQ.7ju2l6d7pKIW3_U456_Szg");
+    // MapboxGL.setConnected(true);
+  }, [])
 
   return <NativeBaseProvider config={config}>
     <SafeAreaView style={{ flex: 1 }}>
