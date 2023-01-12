0246d0be6763834877ac19f714b56b48e86bafa6
andreinegrea14 <andrei.negrea14@yahoo.com>
2023-01-12 05:26:15 +0200

    get notifications endpoint typo fix

--- controllers/get-notifications.js
+++ controllers/get-notifications.js
@@ -3,15 +3,13 @@ const e = require("express");
 const client = require("../configs/database");
 
 exports.getnotifications = async (req, res) => {
-    const {  } = req.body;
+    const { } = req.body;
     try {
         const data = await client.query(`SELECT * FROM notifications;`, []) //Verifying if the user exists in the database
         const notif = data.rows;
-        else {
-            res.status(200).json({
-                notifications: notif
-            });
-        }
+        res.status(200).json({
+            notifications: notif
+        });
     } catch (err) {
         console.log(err);
         res.status(500).json({
