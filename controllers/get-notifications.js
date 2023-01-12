c8b5909df1e75f3781c66bf90dce26650328a879
andreinegrea14 <andrei.negrea14@yahoo.com>
2023-01-12 05:20:17 +0200

    issue fix with empty notifications list case

--- controllers/get-notifications.js
+++ controllers/get-notifications.js
@@ -7,11 +7,6 @@ exports.getnotifications = async (req, res) => {
     try {
         const data = await client.query(`SELECT * FROM notifications;`, []) //Verifying if the user exists in the database
         const notif = data.rows;
-        if (notif.length === 0) {
-            res.status(500).json({
-                error: "Error",
-            });
-        }
         else {
             res.status(200).json({
                 notifications: notif
