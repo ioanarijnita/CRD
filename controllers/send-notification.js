ab322b4c2bc5134a1bf62ce307ca10e199904b23
andreinegrea14 <andrei.negrea14@yahoo.com>
2023-01-12 06:57:40 +0200

    debug messages

--- controllers/send-notification.js
+++ controllers/send-notification.js
@@ -7,14 +7,15 @@ const client = require("../configs/database");
 exports.sendnotification = async (req, res) => {
     const { latitude, longitude, firstname, lastname, email, phonenumber, birthdate, bloodtype, gender, notificationid } = req.body;
     try {
-        client
-            .query(`INSERT INTO notifications (firstname, lastname, email, phonenumber, birthdate, bloodtype, gender, latitude, longitude, notificationid) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);`,
+        console.log("all: ",latitude, longitude, firstname, lastname, email, phonenumber, birthdate, bloodtype, gender, notificationid)
+        client.query(`INSERT INTO notifications (firstname, lastname, email, phonenumber, birthdate, bloodtype, gender, latitude, longitude, notificationid) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);`,
                 [firstname, lastname, email, phonenumber, birthdate, bloodtype, gender, latitude, longitude, notificationid], (err) => {
                     if (err) {
                         res.status(500).json({
                             message: "Error sending the notification!, error: " + err
                         })
                     } else {
+                        console.log("notif: ",notificationid)
                         res.status(200).json({
                             message: "Notification send to authorities!",
                             notificationid: notificationid
