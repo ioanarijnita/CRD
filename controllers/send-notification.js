753c554cb877be5cf8e9bcd319b19c394830f980
andreinegrea14 <andrei.negrea14@yahoo.com>
2023-01-12 07:19:22 +0200

    send notifications debugging

--- controllers/send-notification.js
+++ controllers/send-notification.js
@@ -3,7 +3,10 @@ const client = require("../configs/database");
 
 //Login Function
 exports.sendnotification = async (req, res) => {
-    const { latitude, longitude, firstname, lastname, email, phonenumber, birthdate, bloodtype, gender, notificationid } = req.body;
+    const { id, latitude, longitude, firstname, lastname, email, phonenumber, birthdate, bloodtype, gender, notificationid } = req.body;
+    
+    const data = await client.query(`SELECT * FROM users WHERE id= $1;`, [id]) //Verifying if the user exists in the database
+    console.log(data.rows)
     try {
         console.log("all: ",latitude, longitude, firstname, lastname, email, phonenumber, birthdate, bloodtype, gender, notificationid)
         client.query(`INSERT INTO notifications (firstname, lastname, email, phonenumber, birthdate, bloodtype, gender, latitude, longitude, notificationid) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);`,
